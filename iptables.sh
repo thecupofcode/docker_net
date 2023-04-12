7e0943ebaedc87a30c175ea742d946b7a207d5b1
thecupofcode <93494521+thecupofcode@users.noreply.github.com>
2023-04-12 18:16:50 +0300

    Replace state with conntrack

--- iptables.sh
+++ iptables.sh
@@ -10,7 +10,7 @@ iptables -A FORWARD -s 192.168.221.0/28 -o eth0 -j ACCEPT
 iptables -A FORWARD -s 192.168.221.0/28 -d 192.168.221.0/28 -j ACCEPT
 
 # Accept established/related incoming traffic
-iptables -A FORWARD -d 192.168.221.0/28 -i eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -d 192.168.221.0/28 -i eth0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
 # Nat on eth0 interface
 iptables -t nat -A POSTROUTING -s 192.168.221.0/28 -o eth0 -j MASQUERADE
